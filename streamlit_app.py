b2cb77cfc52f7ddd58f588868f33a33a1b8326b6
Axel RUFFIER DES AIMES <axelruffierdesaimes@gmail.com>
2023-12-18 16:13:21 +0100

    Rename and complete streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import requests
+import pandas as pd
+
+# Streamlit page configuration
+st.title("Bike Accident Probability Prediction")
+
+# Input fields for the user
+address = st.text_input("Enter the address")
+date = st.date_input("Select the date")
+
+if st.button("Predict Accident Probability"):
+    # API endpoint
+    url = "http://localhost:8000/predict"
+
+    # Parameters to send to the FastAPI backend
+    params = {
+        "adr": address,
+        "date": date#.isoformat()  # Convert date to ISO format string
+    }
+
+    # Sending a request to the FastAPI server
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        # Display the prediction result
+        prediction = response.json()
+        accident_probability = prediction['accident_probability']
+        st.write(f"Predicted Accident Probability: {accident_probability}")
+
+        lat, long = prediction['lat'], prediction['long']
+
+        # map centered around the coordinates
+        map_data = pd.DataFrame({'lat': [lat], 'lon': [long]})
+        st.map(map_data)
+    else:
+        st.error("Error in API Call")
